ab3d18fe50a3538a4353d6f53068b102e83e15df
Angel Andrei Espiritu <pacheco.espiritu1718@gmail.com>
2026-04-09 23:52:27 -0600

    style: add standardized format with Input/Output comments for each function

--- algorithms/arrays/array-unique-number/arrayUniqueNumber.js
+++ algorithms/arrays/array-unique-number/arrayUniqueNumber.js
@@ -12,10 +12,16 @@ function getUniqueNumbers(arr){
     return uniqueNumbers;
 }
 
+// Input: hasDuplicates([1, 2, 2, 3])
+// Output: true
+
 function hasDuplicates(arr){
     if(new Set(arr).size === arr.length) return false;
     return true; 
 }
+
+// Input: getFirstDuplicate([2, 1, 3, 1, 2])
+// Output: 1
 
 function getFirstDuplicate(arr){
     const seen = new Set();
@@ -26,10 +32,16 @@ function getFirstDuplicate(arr){
     return null;
 }
 
+// Input: getArrayIntersection([1, 2, 3], [2, 3, 4])
+// Output: [2, 3]
+
 function getArrayIntersection(arr, arr2){
     const b = new Set(arr2);
     return [...new Set(arr.filter(num => b.has(num)))];
 }
+
+// Input: getArrayDifference([1, 2, 3], [2, 3, 4])
+// Output: [1, 4]
 
 function getArrayDifference(arr, arr2){
     return [...new Set([...arr.filter(num => !arr2.includes(num)), ...arr2.filter(num => !arr.includes(num))])]; 
