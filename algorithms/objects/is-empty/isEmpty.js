8b3620c46e2843f92c85582e1748f5437fde9bad
Angel Andrei Espiritu <pacheco.espiritu1718@gmail.com>
2026-04-17 23:04:02 -0600

    style: add missing Input/Output comments for isEmpty functions

--- algorithms/objects/is-empty/isEmpty.js
+++ algorithms/objects/is-empty/isEmpty.js
@@ -4,8 +4,8 @@
  * Difficulty: Easy
  */
 
-// Input: isEmptyByJSON({1:'a', 2:'b', 3:'c', 4:'d', 5:'e'})
-// Output: false
+// Input: isEmptyByFor({})
+// Output: true
 
 function isEmptyByFor(obj){
     for(let i in obj){
@@ -14,9 +14,15 @@ function isEmptyByFor(obj){
     return true;
 }
 
+// Input: isEmptyByKeys({ a: 1, b: 2 })
+// Output: false
+
 function isEmptyByKeys(obj){
     return Object.keys(obj).length === 0;
 }
+
+// Input: isEmptyByJSON({1:'a', 2:'b', 3:'c', 4:'d', 5:'e'})
+// Output: false
 
 function isEmptyByJSON(obj){
     return JSON.stringify(obj) === '{}';
